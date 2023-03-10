e16ba0af45ed3601b8bc77d38610ae57c6e4e7cb
Emma Stoudemire <Estoudemire@smith.edu>
2023-03-09 20:53:50 -0500

    Engine Attributes + constructor

--- Engine.java
+++ Engine.java
@@ -1,3 +1,36 @@
 public class Engine {
+    
+    //Variables for the class
+    private String fuelType; 
+    private double maxFuel; 
+    private double currentFuel; 
 
+    //Constructor
+    public Engine (String fuelType, double maxFuel, double currentFuel){
+        this.fuelType = fuelType; 
+        this.maxFuel = maxFuel; 
+        this.currentFuel = currentFuel; 
+    }
+
+    //Accessor methods 
+    public void setFuelType (String newFuelType){
+        this.fuelType = newFuelType; 
+    }
+    public String getFuelType (){
+        return fuelType; 
+    }
+
+    public void setMaxFuel (double maxFuel){
+        this.maxFuel = maxFuel; 
+    }
+    public double getMaxFuel (){
+        return maxFuel; 
+    }
+
+    public void setCurrentFuel (double currentFuel){
+        this.currentFuel = currentFuel; 
+    }
+    public double getCurrentFuel (){
+        return currentFuel; 
+    }
 }
